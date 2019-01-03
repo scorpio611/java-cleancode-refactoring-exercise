220d4c6209140789904e7e428dba18c8684408b6
scorpio611 <evilmonter666@gmail.com>
2019-01-04 00:07:14 +0700

    [Bài tập] Refactoring

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,60 +1,76 @@
 public class TennisGame {
+    private static String score = "";
+    private static final char Love = 0;
+    private static final char Fifteen = 1;
+    private static final char Thirty = 2;
+    private static final char Forty = 3;
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+    private void DeuceScore(int playerScore){
+        switch (playerScore)
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+            case Love:
+                score = "Love-All";
+                break;
+            case Fifteen:
+                score = "Fifteen-All";
+                break;
+            case Thirty:
+                score = "Thirty-All";
+                break;
+            case Forty:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+    }
 
+    private void WinScore(int player1Score, int player2Score,String player1Name, String player2Name){
+        int Result = player1Score-player2Score;
+        if (Result==1) score ="Advantage "+player1Name;
+        else if (Result ==-1) score ="Advantage "+player2Name;
+        else if (Result>=2) score = "Win for "+player1Name;
+        else score ="Win for "+player2Name;
+    }
+
+    private void Score(int player1Score, int player2Score){
+        for (int player=1; player<3; player++)
+        {
+            int tempScore = 0;
+            if (player==1) tempScore = player1Score;
+            else { score+="-"; tempScore = player2Score;}
+            switch(tempScore)
+            {
+                case Love:
+                    score+="Love";
+                    break;
+                case Fifteen:
+                    score+="Fifteen";
+                    break;
+                case Thirty:
+                    score+="Thirty";
+                    break;
+                case Forty:
+                    score+="Forty";
+                    break;
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+    }
+
+    public String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+
+        if (player1Score==player2Score)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            DeuceScore(player1Score);
+        }
+        else if (player1Score>=4 || player2Score>=4)
+        {
+            WinScore(player1Score, player2Score, player1Name,  player2Name);
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            Score(player1Score,player2Score);
         }
         return score;
     }
